aefdd94d2785709657d8660e9d621af03db37717
ayrtoncatri <ayrtoncatri@gmail.com>
2025-11-28 01:46:38 -0300

    feat: update firebase configuration to use environment variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyA-XO-Heo8EZvYi4GSU5Y10HIufcLYwzCA",
-  authDomain: "firechat-droner.firebaseapp.com",
-  projectId: "firechat-droner",
-  storageBucket: "firechat-droner.firebasestorage.app",
-  messagingSenderId: "1027507739294",
-  appId: "1:1027507739294:web:5e1fc8607c4a482a58798f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
